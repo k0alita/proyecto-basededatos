6f6cd611bb22f0755b9e43bb38c7e914be1da48e
Jav8-Dev <javiergarrochomunoz@gmail.com>
2026-05-08 10:09:43 +0200

    holi

--- src/main/java/dao/ConexionDB.java
+++ src/main/java/dao/ConexionDB.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexionDB {
-    private static final String URL = "jdbc:mariadb://localhost:3306/robuxgames2";
-    private static final String USER = "root";
-    private static final String PASS = "root";
+    private static final String URL = "jdbc:mariadb://34.163.161.32:3306/robuxgames2";
+    private static final String USER = "robux_user";
+    private static final String PASS = "Bermudo123";
 
     public static Connection getConnection() throws SQLException {
         try {
